8636b02875ecce9b95d6b6c3149b59edc40624a3
ledroid <leandro.hdsl@gmail.com>
2017-05-22 18:57:18 -0300

    Atualização de Activity e Select

--- app/src/main/java/zap/com/example/app/appzap/AplicationActivity.java
+++ app/src/main/java/zap/com/example/app/appzap/AplicationActivity.java
@@ -5,6 +5,7 @@ import android.os.AsyncTask;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
+import android.view.View;
 import android.widget.*;
 import org.json.JSONArray;
 import org.json.JSONException;
@@ -13,8 +14,9 @@ import zap.com.example.app.appzap.controller.ServiceRest;
 
 import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.List;
 
-public class AplicationActivity extends AppCompatActivity {
+public class AplicationActivity extends AppCompatActivity implements AdapterView.OnItemSelectedListener {
 
     private String TAG = AplicationActivity.class.getSimpleName();
     //URL REST.
@@ -28,6 +30,10 @@ public class AplicationActivity extends AppCompatActivity {
     //Classe REST para realizar o acesso ao URL com os dados.
     private ServiceRest service;
 
+    private Spinner spinnerItens;
+    private ArrayAdapter<String> dataAdapter;
+    private List<String> categorias;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -36,7 +42,29 @@ public class AplicationActivity extends AppCompatActivity {
         imovelList = new ArrayList<>();
         list = (ListView) findViewById(R.id.list);
 
+        spinnerItens = (Spinner) findViewById(R.id.spinnerItens);
+        spinnerItens.setOnItemSelectedListener(this);
+        // Spinner Drop down elements
+        categorias = new ArrayList<String>();
+        categorias.add("Select ");
+        //categorias.add("Apartamento");
+//        categorias.add("Casa");
+//        categorias.add("Kitnet");
+//        categorias.add("JK");
+//        categorias.add("Loja");
+        dataAdapter = new ArrayAdapter<String>(this, android.R.layout.simple_spinner_item, categorias);
+        spinnerItens.setAdapter(dataAdapter);
         new GetRequest().execute();
+
+    }
+
+    @Override
+    public void onItemSelected(AdapterView<?> adapterView, View view, int i, long l) {
+
+    }
+
+    @Override
+    public void onNothingSelected(AdapterView<?> adapterView) {
 
     }
 
@@ -56,80 +84,84 @@ public class AplicationActivity extends AppCompatActivity {
             service = new ServiceRest();
 
             String jsonString = service.serviceCall(url);
-            //Log.v(TAG, "jsonString URL <? @=@ ?>: " + jsonString);// Mostra a Url do Rest
-            Log.e(TAG, "Resposnse from URL: "+ jsonString);
-            if(jsonString != null){
+            //Log.v(TAG, "jsonString URL : " + jsonString);// Mostra a Url do Rest
+            Log.e(TAG, "Resposnse from URL: " + jsonString);
+
+            if (jsonString != null) {
                 try {
-                        JSONObject jsonObject = new JSONObject(jsonString);
-                        //Log.v(TAG, "jsonObject String : " + jsonObject);// Objeto com a String da URL
-                        JSONArray itens = jsonObject.getJSONArray("Imoveis");
-                        //Log.v(TAG, "itens do Objeto : " + itens);// Itens que contem o objeto
-                        for (int i=0; i< itens.length(); i++){
-                            JSONObject jObject = itens.getJSONObject(i);
-                            Log.i("TAG i >","Objeto do for : \n" + i);
-                            //Log.v(TAG, "jObject subItens do Objeto : " + itens.getJSONObject(i));// SubItens do Objeto
-                            //int codImovel  = jObject.getInt("CodImovel");
-                            String tipoImovel = jObject.getString("TipoImovel");
-                            JSONObject endereco   = jObject.getJSONObject("Endereco");
-                            String numero     = endereco.getString("Numero");
-                            String cep        = endereco.getString("CEP");
-                            String bairro     = endereco.getString("Bairro");
-                            String cidade     = endereco.getString("Cidade");
-                            String estado     = endereco.getString("Estado");
-                            String zona       = endereco.getString("Zona");
-                            int precoVenda = jObject.getInt("PrecoVenda");
-                            int dormitorios = jObject.getInt("Dormitorios");
-                            int suites     = jObject.getInt("Suites");
-                            int vagas      = jObject.getInt("Vagas");
-                            int areaUtil   = jObject.getInt("AreaUtil");
-                            int areaTotal  = jObject.getInt("AreaTotal");
-                            String dataAtualizacao = jObject.getString("DataAtualizacao");
-                            JSONObject cliente     = jObject.getJSONObject("Cliente");
-                            String codCliente = cliente.getString("CodCliente");
-                            String nomeFantasia = cliente.getString("NomeFantasia");
+                    JSONObject jsonObject = new JSONObject(jsonString);
+                    //Log.v(TAG, "jsonObject String : " + jsonObject);// Objeto com a String da URL
+                    JSONArray itens = jsonObject.getJSONArray("Imoveis");
+                    //Log.v(TAG, "itens do Objeto : " + itens);// Itens que contem o objeto
 
-                            HashMap<String, String> imoveis = new HashMap<>();
-                            //imoveis.put("CodImovel", String.valueOf(codImovel));
-                            imoveis.put("TipoImovel", tipoImovel);
-                            imoveis.put("Endereco", String.valueOf(endereco));
-                            imoveis.put("Numero", numero);
-                            imoveis.put("CEP", cep);
-                            imoveis.put("Bairro", bairro);
-                            imoveis.put("Cidade", cidade);
-                            imoveis.put("Estado", estado);
-                            imoveis.put("Zona", zona);
-                            imoveis.put("PrecoVenda", String.valueOf(precoVenda));
-                            imoveis.put("Dormitorios", String.valueOf(dormitorios));
-                            imoveis.put("Suites", String.valueOf(suites));
-                            imoveis.put("Vagas", String.valueOf(vagas));
-                            imoveis.put("AreaUtil", String.valueOf(areaUtil));
-                            imoveis.put("AreaTotal", String.valueOf(areaTotal));
-                            imoveis.put("DataAtualizacao", dataAtualizacao);
-                            imoveis.put("Cliente", String.valueOf(cliente));
-                            imoveis.put("CodCliente", codCliente);
-                            imoveis.put("NomeFantasia", nomeFantasia);
+                    for (int i = 0; i < itens.length(); i++) {
+                        JSONObject jObject = itens.getJSONObject(i);
+                        Log.i("TAG i >", "Objeto do for : \n" + i);
+                        //Log.v(TAG, "jObject subItens do Objeto : " + itens.getJSONObject(i));// SubItens do Objeto
+                        //int codImovel  = jObject.getInt("CodImovel");
+                        String tipoImovel = jObject.getString("TipoImovel");
+                        categorias.add(tipoImovel);
+                        JSONObject endereco = jObject.getJSONObject("Endereco");
+                        String numero = endereco.getString("Numero");
+                        String cep = endereco.getString("CEP");
+                        String bairro = endereco.getString("Bairro");
+                        String cidade = endereco.getString("Cidade");
+                        String estado = endereco.getString("Estado");
+                        String zona = endereco.getString("Zona");
+                        int precoVenda = jObject.getInt("PrecoVenda");
+                        int dormitorios = jObject.getInt("Dormitorios");
+                        int suites = jObject.getInt("Suites");
+                        int vagas = jObject.getInt("Vagas");
+                        int areaUtil = jObject.getInt("AreaUtil");
+                        int areaTotal = jObject.getInt("AreaTotal");
+                        String dataAtualizacao = jObject.getString("DataAtualizacao");
+                        JSONObject cliente = jObject.getJSONObject("Cliente");
+                        String codCliente = cliente.getString("CodCliente");
+                        String nomeFantasia = cliente.getString("NomeFantasia");
 
-                            imovelList.add(imoveis);
-                        }
-                    } catch (final JSONException e) {
-                        Log.e(TAG,"JSON Erro Parsin " + e.getMessage());
-                        runOnUiThread(new Runnable() {
-                            @Override
-                            public void run() {
-                                Toast.makeText(getApplicationContext(), "Erro JSON Parsin !\n" + e.getMessage(), Toast.LENGTH_LONG).show();
-                            }
-                        });
-                        e.printStackTrace();
+                        HashMap<String, String> imoveis = new HashMap<>();
+                        //imoveis.put("CodImovel", String.valueOf(codImovel));
+                        imoveis.put("TipoImovel", tipoImovel);
+                        //imoveis.put("Endereco", String.valueOf(endereco.getString("Cidade")));
+                        imoveis.put("Numero", numero);
+                        imoveis.put("CEP", cep);
+                        imoveis.put("Bairro", bairro);
+                        imoveis.put("Cidade", cidade);
+                        imoveis.put("Estado", estado);
+                        imoveis.put("Zona", zona);
+                        imoveis.put("PrecoVenda", String.valueOf(precoVenda));
+                        imoveis.put("Dormitorios", String.valueOf(dormitorios));
+                        imoveis.put("Suites", String.valueOf(suites));
+                        imoveis.put("Vagas", String.valueOf(vagas));
+                        imoveis.put("AreaUtil", String.valueOf(areaUtil));
+                        imoveis.put("AreaTotal", String.valueOf(areaTotal));
+                        //imoveis.put("DataAtualizacao", dataAtualizacao);
+                        imoveis.put("Cliente", String.valueOf(cliente.getString("NomeFantasia")));
+                        imoveis.put("CodCliente", codCliente);
+                        //imoveis.put("NomeFantasia", nomeFantasia);
+
+                        imovelList.add(imoveis);
+
                     }
-                } else {
-                    Log.e(TAG, "Not did get json server." );
+                } catch (final JSONException e) {
+                    Log.e(TAG, "JSON Erro Parsin " + e.getMessage());
                     runOnUiThread(new Runnable() {
                         @Override
                         public void run() {
-                            Toast.makeText(getApplicationContext(), "Not did get json server. ", Toast.LENGTH_LONG).show();
+                            Toast.makeText(getApplicationContext(), "Erro JSON Parsin !\n" + e.getMessage(), Toast.LENGTH_LONG).show();
                         }
                     });
-             }
+                    e.printStackTrace();
+                }
+            } else {
+                Log.e(TAG, "Not did get json server.");
+                runOnUiThread(new Runnable() {
+                    @Override
+                    public void run() {
+                        Toast.makeText(getApplicationContext(), "Not did get json server. ", Toast.LENGTH_LONG).show();
+                    }
+                });
+            }
 
             return null;
         }
@@ -144,14 +176,14 @@ public class AplicationActivity extends AppCompatActivity {
                     "TipoImovel", "Endereco", "Numero",
                     "CEP", "Bairro", "Cidade",
                     "Estado", "Zona", "PrecoVenda",
-                    "Dormitorios", "Suites", "AreaUtil",
+                    "Dormitorios", "Suites", "Vagas", "AreaUtil",
                     "AreaTotal", "DataAtualizacao",
                     "CodCliente", "Nomefantasia",
                     "Cliente", "CodCliente", "NomeFantasia"}, new int[]{/*R.id.codIovel,*/
                     R.id.tipoImovel, R.id.endereco, R.id.numero,
-                    R.id.cep,  R.id.bairro, R.id.cidade,
+                    R.id.cep, R.id.bairro, R.id.cidade,
                     R.id.estado, R.id.zona, R.id.precoVenda,
-                    R.id.dormitorios, R.id.suites, R.id.areaUtil,
+                    R.id.dormitorios, R.id.suites, R.id.vagas, R.id.areaUtil,
                     R.id.areaTotal, R.id.dataAtualizacao,
                     R.id.codCliente, R.id.nomeFantasia,
                     R.id.cliente, R.id.codCliente, R.id.nomeFantasia});
